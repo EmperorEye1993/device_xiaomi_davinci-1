ArianK16a/android_device_xiaomi_davinci

davinci: Get rid of megvii dependencies in camera.qcom.so
Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
Signed-off-by: Arian <arian.kulmer@web.de>

extract-files.sh
@@ -67,6 +67,11 @@ function blob_fixup() {
     lib64/libfm-hci.so)
         patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
         ;;
+    vendor/lib64/hw/camera.qcom.so)
+        patchelf --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
+        patchelf --remove-needed "libmegface.so" "${2}"
+        patchelf --add-needed "libshim_camera.so" "${2}"
+        ;;
     esac
 }
 

EmperorEye1993/device_xiaomi_davinci-1

extract-files.sh
@@ -67,6 +67,11 @@ function blob_fixup() {
     lib64/libfm-hci.so)
         patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
         ;;
+    vendor/lib64/hw/camera.qcom.so)
+        patchelf --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
+        patchelf --remove-needed "libmegface.so" "${2}"
+        patchelf --add-needed "libshim_megvii.so" "${2}"
+        ;;
     esac
 }
 